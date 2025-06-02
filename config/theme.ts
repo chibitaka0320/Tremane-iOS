8cd790b6ff8a3b935176cb67dbcdd14eb24e8838
chibitaka0320 <taka.araat32@gmail.com>
2025-06-02 22:27:09 +0900

    スタイルテーマ作成

---
+++ config/theme.ts
@@ -0,0 +1,26 @@
+// スペース
+const spacing = [2, 4, 8, 16, 32, 64, 128, 256, 512];
+
+// 色
+const colors = {
+  primary: "#72D2FF",
+  background: {
+    light: "#FFFFFF",
+    main: "#E4E4E4",
+  },
+};
+
+// 文字サイズ
+const fontSizes = {
+  small: 12,
+  midium: 16,
+  large: 20,
+};
+
+const theme = {
+  colors,
+  fontSizes,
+  spacing,
+};
+
+export default theme;
