0cc653c50bdb933f15f9af948c9b7e3514da4442
chibitaka0320 <taka.araat32@gmail.com>
2025-06-07 14:18:55 +0900

    ログイン自動認証処理作成

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,39 @@
 import { Redirect } from "expo-router";
-
-const isAuthenticated = false;
+import { useEffect, useState } from "react";
+import * as SecureStore from "expo-secure-store";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 export default function Index() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      try {
+        const token = await SecureStore.getItemAsync("userToken");
+        setIsAuthenticated(token !== null);
+      } catch (e) {
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkLogin();
+  }, []);
+
+  if (isAuthenticated === null) {
+    return (
+      <View style={styles.active}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
   return <Redirect href={isAuthenticated ? "/training" : "/auth/signIn"} />;
 }
+
+const styles = StyleSheet.create({
+  active: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
