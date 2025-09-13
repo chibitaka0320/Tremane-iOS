80e1bfb73a831ceb52e5c0c638862551323eabbb
chibitaka0320 <taka.araat32@gmail.com>
2025-09-13 10:20:41 +0900

    既読未読のアイコン修正

--- app/(main)/(tabs)/_layout.tsx
+++ app/(main)/(tabs)/_layout.tsx
@@ -10,14 +10,56 @@ import theme from "@/styles/theme";
 
 import AnalysisScreen from "./analysis";
 import { router, useNavigation } from "expo-router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import MainScreen from "./(home)/main";
 import FriendTabs from "./friend";
+import { apiRequestWithRefresh } from "@/lib/apiClient";
 
 const BottomTab = createBottomTabNavigator();
 
 export default function TabsLayout() {
   const navigation = useNavigation();
+  const [count, setCount] = useState(0);
+
+  // 未読件数取得
+  const getNonNotificationCount = async () => {
+    try {
+      const response = await apiRequestWithRefresh(
+        "/notifications/noread",
+        "GET"
+      );
+      if (response?.ok) {
+        const countStr = await response.text();
+        setCount(Number(countStr));
+      } else {
+        console.error("APIレスポンスエラー" + response?.status);
+      }
+    } catch (error) {
+      console.error("API取得エラー：" + error);
+    }
+  };
+
+  // 通知を既読化
+  const markReadAll = async () => {
+    try {
+      const response = await apiRequestWithRefresh(
+        "/notifications/read",
+        "PUT"
+      );
+      if (response?.ok) {
+        setCount(0);
+      } else {
+        console.error("APIレスポンスエラー" + response?.status);
+      }
+    } catch (error) {
+      console.error("API取得エラー：" + error);
+    }
+  };
+
+  // 未読件数取得（初期表示時）
+  useEffect(() => {
+    getNonNotificationCount();
+  }, []);
 
   const onMenu = () => {
     router.push("/(main)/(menu)/menu");
@@ -26,6 +68,7 @@ export default function TabsLayout() {
   // 通知画面遷移
   const onNotification = () => {
     router.push("/notification");
+    markReadAll();
   };
 
   return (
@@ -53,6 +96,19 @@ export default function TabsLayout() {
                   size={25}
                   color="black"
                 />
+                {count > 0 && (
+                  <View
+                    style={{
+                      position: "absolute",
+                      right: 1,
+                      top: 1,
+                      backgroundColor: "red",
+                      borderRadius: "50%",
+                      width: 10,
+                      height: 10,
+                    }}
+                  ></View>
+                )}
               </TouchableOpacity>
               <TouchableOpacity onPress={onMenu} style={{ marginRight: 16 }}>
                 <Feather name="menu" size={25} />
