026e886b4bb659bd1a460dee28832ce483ae7877
chibitaka0320 <taka.araat32@gmail.com>
2025-09-27 12:39:11 +0900

    微修正

--- types/api.ts
+++ types/api.ts
@@ -136,28 +136,6 @@ export type ExerciseRequest = {
   updatedAt: string;
 };
 
-// ユーザー情報API
-export type UserInfoResponse = {
-  height: number;
-  weight: number;
-  birthday: Date;
-  age: number;
-  gender: number;
-  activeLevel: number;
-  bmr: number;
-  totalCalorie: number;
-};
-
-// ユーザー目標API
-export type UserGoalDto = {
-  weight: number;
-  goalWeight: number;
-  goalCalorie: number;
-  start: Date;
-  finish: Date;
-  pfc: number;
-};
-
 // ユーザーアカウント情報API
 export type UserAccountInfoResponse = {
   userId: string;
